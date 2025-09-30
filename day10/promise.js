9ec5cbc4ae84cfa8c8f98676c3254c0cbe81a28c
rv-unq <ruvishushukla@gmail.com>
2025-09-30 15:21:10 +0530

    Day10

--- day10/promise.js
+++ day10/promise.js
@@ -2,23 +2,29 @@ const printMessage =(msg)=>{
     console.log(msg);
 }
 const wait = (msg,sec)=>{
-    setTimeout(()=>{
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
         printMessage(msg);
+        resolve();
     },sec);
+    });
 }
+// const bookTicket=()=>{
+//     printMessage("Welcome");
+//     wait("Login Sucessfully",3000)
+//     .then(()=>wait("seat has been selected sucessfully",2000))
+//     .then(()=>wait("your seat has been booked",3000))
+//     .then(()=>wait("your seat has been booked",4000))
+//     .then(()=>wait("Ticket has been confirmed",3000))
+//     .then(()=>printMessage("Thankyou"))
+// }
 
 
-const bookTicket=()=>{
+const bookTicket=async()=>{
     printMessage("Welcome");
-    wait("Login Sucessfully",3000);
-    wait("seat has been selected sucessfully",4000);
-    wait("your seat has been booked",3000);
-    wait("Payment has been done",4000);
-    wait("Ticket has been confirmed",3000);
+    await wait("Login Sucessfully",3000);
+    await wait("seat has been selected sucessfully",4000);
+    await wait("your seat has been booked",3000);
+    await wait("Payment has been done",4000);
+    await wait("Ticket has been confirmed",3000);
 }
-
-
-
-
-
-
